718296fe9aa4b8171a6d0df9c84384deeb7cb58e
Jonathan Boos <gameguy37@gmail.com>
2019-04-11 20:54:41 -0400

    Create 3 homework files

---
+++ W1D5/map.rb
@@ -0,0 +1,24 @@
+class Map
+
+    def set(key, value)
+        #Note that the set method can be used to either create a
+        #new key-value pair or update the value for a pre-existing key.
+        #It's up to you to check whether a key currently exists!
+
+        
+    end
+
+    def get(key)
+
+    end
+
+    def delete(key)
+
+    end
+
+    def show
+
+    end
+
+
+end
---
+++ W1D5/queue.rb
@@ -0,0 +1,15 @@
+class Queue
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
+
+    end
+
+    def peek
+
+    end
+
+end
