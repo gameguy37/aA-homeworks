8b2eede449058017607d714fb2c5ef4f89422e93
Jonathan Boos <gameguy37@gmail.com>
2019-04-24 23:13:26 -0400

    W3D3 homework

---
+++ W3D3/nplus1_skeleton/app/models/route.rb
@@ -0,0 +1,45 @@
+class Route < ApplicationRecord
+  has_many :buses,
+    class_name: 'Bus',
+    foreign_key: :route_id,
+    primary_key: :id
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    # TODO: your code here
+    # drivers = Hash.new { [] }
+    
+    # buses = self.buses.includes(:drivers)
+
+    # buses.each do |bus|
+    #   drivers[bus.id] << bus.drivers.name
+    # end
+    # drivers
+    all_drivers = {}
+
+    buses = self.buses.includes(:drivers)
+
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+    all_drivers
+  end
+end
